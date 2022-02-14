a8ca8c3874068bf5709f592bdf0531f709050858
TheMDRobinson <matthewdrobinson@gmail.com>
2022-02-14 13:24:12 +0100

    first commit

---
+++ js/services/books.js
@@ -0,0 +1,9 @@
+app.factory('books', ['$http', function($http) {
+    return $http.get('https://content.codecademy.com/courses/ltp4/books-api/books.json')
+    .success(function(data) {
+      return data;
+    })
+    .error(function(err) {
+      return err;
+    });
+  }]);
